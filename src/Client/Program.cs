ab0ec5959011d0e47be66c049a0930d6db536105
Diego Browen Proença de Oliveira <diego@DESKTOP-GMKS0LP>
2019-05-31 17:28:35 -0300

    Add project files.

---
+++ src/Client/Program.cs
@@ -0,0 +1,64 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var tokenResponse = await GetToken();
+            using (var client = new HttpClient())
+            {
+                client.SetBearerToken(tokenResponse.AccessToken);
+
+                var response = await client.GetAsync("http://localhost:5001/identity");
+                if (!response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine(response.StatusCode);
+                }
+                else
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(JArray.Parse(content));
+                }
+            }
+
+        }
+
+        private static async Task<TokenResponse> GetToken()
+        {
+            using (var client = new HttpClient())
+            {
+                var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
+                if (disco.IsError)
+                {
+                    Console.WriteLine(disco.Error);
+                    return null;
+                }
+
+                var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+                {
+                    Address = disco.TokenEndpoint,
+                    
+                    ClientId = "client",
+                    ClientSecret = "secret",
+                    Scope = "api1"
+                });
+
+                if (tokenResponse.IsError)
+                {
+                    Console.WriteLine(tokenResponse.Error);
+                    return null;
+                }
+
+                Console.WriteLine(tokenResponse.Json);
+                return tokenResponse;
+            }
+
+        }
+    }
+}
