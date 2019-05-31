0fe39ebfac5bbdb96b2ce7193408c3b078289ea2
Diego Browen Proença de Oliveira <diego@DESKTOP-GMKS0LP>
2019-05-31 17:39:28 -0300

    Adicionado Logout

--- src/WebApplication/Controllers/HomeController.cs
+++ src/WebApplication/Controllers/HomeController.cs
@@ -21,6 +21,10 @@ namespace WebApplication.Controllers
         {
             return View();
         }
+        public IActionResult Logout()
+        {
+            return SignOut("Cookies", "oidc");
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
