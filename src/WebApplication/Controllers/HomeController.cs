81834848f1e2b49951b590997eecc832203ca1a9
Diego Browen Proença de Oliveira <diego@DESKTOP-GMKS0LP>
2019-06-01 22:25:00 -0300

    Criado acesso do projeto web para acessar api, atravez do token de autenticação

--- src/WebApplication/Controllers/HomeController.cs
+++ src/WebApplication/Controllers/HomeController.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 using WebApplication.Models;
 
 namespace WebApplication.Controllers
@@ -30,6 +34,19 @@ namespace WebApplication.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public async Task<IActionResult> CallApi()
+        {
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+                var content = await client.GetStringAsync("http://localhost:5001/identity");
+
+                ViewBag.Json = JArray.Parse(content).ToString();
+                return View("json");
+            }
         }
     }
 }
